fcbe29e72d11293ace019c897827de33d0b4bdb7
AlinaIunda <122435971+AlinaIunda@users.noreply.github.com>
2023-03-15 10:13:21 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-// Функция, которая принимает на вход два числа и возвращает их сумму:
+
 function sum(num1, num2) {
   return num1 + num2;
 }
 
 console.log(sum(5, 7))
 
-//Функция, которая принимает на вход строку и возвращает ее длину:
+
 function stringLength(str) {
   return str.length;
 }
