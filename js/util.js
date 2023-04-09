97cdecd4c4f63af84588f54deb811967f30ded80
AlinaIunda <122435971+AlinaIunda@users.noreply.github.com>
2023-04-09 12:06:26 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,30 +1,6 @@
-export function getRandomInt(from, to) {
-  if (from < 0 || to < 0) {
-    throw new RangeError('Числа в диапазоне должны быть положительными');
-  }
 
-  if (from === to) {
-    return from;
-  }
-  if (from > to) {
-    [from, to] = [to, from];
-  }
 
-  return Math.round(Math.random() * (to - from) + from);
-}
+const getRandomNumber = (min, max) => (Math.floor(Math.random() * (max - min) + min));
+// const checkStringLength = (str, maxLength) => (str.length <= maxLength);
 
-export const isCorrectLength = (str, maxLength) => str.length <= maxLength;
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+export {getRandomNumber};
