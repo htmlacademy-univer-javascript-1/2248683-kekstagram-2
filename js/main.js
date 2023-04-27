226bcc742188670134e1a91a2a2c70621aec5925
AlinaIunda <122435971+AlinaIunda@users.noreply.github.com>
2023-04-27 10:56:29 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,13 +5,20 @@ import {showFilters} from './filters.js';
 
 //const descriptions = Array.from({length: PHOTOS_COUNT}, generateDescription);
 //insertPhotoMiniature(descriptions);
-
+const fileInput = document.querySelector('#upload-file');
+const preview = document.querySelector('.img-upload__preview').querySelector('img');
 
 getMiniatures()
- 
   .then((r) => {
     insertPhotoMiniature(r);
-    return r;
-  })
-  .then(showFilters);
-document.querySelector('#upload-file').addEventListener('change', showFileForm);
+    showFilters(r);
+  });
+
+fileInput.addEventListener('change', () => {
+  const file = fileInput.files[0];
+  if (!['png', 'jpg', 'jpeg'].some((suf) => file.name.toLowerCase().endsWith(suf))){
+    return;
+  }
+  preview.src = URL.createObjectURL(file);
+  showFileForm();
+});
